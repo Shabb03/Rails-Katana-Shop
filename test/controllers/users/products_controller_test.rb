9a81fddc03a4a9f1173b9b42531dd05f37b4afcd
shabb03 <rishab.sidhu0@gmail.com>
2025-01-17 22:39:56 +0000

    Removed all zone identifier files

--- test/controllers/users/products_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Users::ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_products_show_url
-    assert_response :success
-  end
-end
